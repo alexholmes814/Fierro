619f0899387e44ec0de5ccd28fc792985add1810
alexholmes814 <alexanderholmes777@gmail.com>
2026-02-16 16:53:04 -0600

    comment clean up, removed debug functions and unused variables

--- app/src/Solvers/SGH_solver_3D/include/fracture.h
+++ app/src/Solvers/SGH_solver_3D/include/fracture.h
@@ -1,25 +1,42 @@
+/**********************************************************************************************
+© 2020. Triad National Security, LLC. All rights reserved.
+This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos
+National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S.
+Department of Energy/National Nuclear Security Administration. All rights in the program are
+reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear
+Security Administration. The Government is granted for itself and others acting on its behalf a
+nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare
+derivative works, distribute copies to the public, perform publicly and display publicly, and
+to permit others to do so.
+This program is open source under the BSD-3 License.
+Redistribution and use in source and binary forms, with or without modification, are permitted
+provided that the following conditions are met:
+1.  Redistributions of source code must retain the above copyright notice, this list of
+conditions and the following disclaimer.
+2.  Redistributions in binary form must reproduce the above copyright notice, this list of
+conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+3.  Neither the name of the copyright holder nor the names of its contributors may be used
+to endorse or promote products derived from this software without specific prior
+written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************/
 #ifndef Fracture_H
 #define Fracture_H
 #include "matar.h"
 #include "mesh.h"
 #include "state.h"
 #include "simulation_parameters.h"
-
-// struct for fracture nodes
-struct fracture_nodes_t {
-    size_t gid; // global node id
-    double mass; // mass of the node
-    CArrayKokkos<double> pos = CArrayKokkos<double>(3);  // position of the node
-    CArrayKokkos<double> vel = CArrayKokkos<double>(3);  // velocity of the node
-    //CArrayKokkos<double> internal_force = CArrayKokkos<double>(3);  // any force that is not due to fracture
-    //CArrayKokkos<double> fracture_force = CArrayKokkos<double>(3);  // force due to fracture
-
-    fracture_nodes_t();
-
-    fracture_nodes_t(const ViewCArrayKokkos<double> &pos, const ViewCArrayKokkos<double> &vel,
-                   const ViewCArrayKokkos<double> &internal_force, const ViewCArrayKokkos<double> &fracture_force,
-                   const double &mass);
-};
 
 // struct for fracture cohesive zones
 
@@ -37,7 +54,6 @@ struct cohesive_zones_t {
     size_t max_elem_in_cohesive_zone;
     CArrayKokkos<double> internal_vars_n0; // 1/28/2026 addition: storage for internal vars at t_n
 
-    
 
     // Iso-parametric coordinates of the patch nodes (1D array of size mesh.num_nodes_in_surf)
     // For a standard linear hex, xi = [-1.0, 1.0, 1.0, -1.0], eta = [-1.0, -1.0, 1.0, 1.0]
@@ -53,57 +69,6 @@ struct cohesive_zones_t {
     // would look something like void node_pairs_t::initialize(const Mesh_t &mesh, ...)
     // this is where the algorithim to find the unique node pairs (boundary nodes) will go
     // only thing that should be in sgh_setup.cpp is calling this function
-
-    // START OF DEBUG FUNCTION DECLARATIONS
-
-    // debug_oriented()
-    void debug_oriented(Mesh_t& mesh,
-                        State_t& State,
-                        CArrayKokkos<size_t>& overlap,
-                        CArrayKokkos<int>& info,
-                        size_t maxcz,
-                        double tol);
-    
-    // debug_ucmap()                    
-    void debug_ucmap(
-        const DCArrayKokkos<double>& pos,
-        //const DCArrayKokkos<double>& pos,
-        const DCArrayKokkos<double>& vel,                 
-        double dt,
-        const CArrayKokkos<double>& cohesive_zone_orientation,
-        const CArrayKokkos<size_t>& overlapping_node_gids,
-        const CArrayKokkos<double>& local_opening
-    );
-
-    // debug_cohesive_zone_var_update()
-    KOKKOS_FUNCTION
-    void debug_cohesive_zone_var_update(
-        const CArrayKokkos<double>& local_opening,
-        const double dt,
-        const double time_value, // ADDED IN FOR DEBUGGING
-        const CArrayKokkos<size_t>& overlapping_node_gids,
-        const RaggedRightArrayKokkos<double>& stress_bc_global_vars,
-        const int bdy_set,
-        const ViewCArrayKokkos<double>& internal_vars,      // (overlapping_node_gids.dims(0), 4 + num_prony_terms)
-        const ViewCArrayKokkos<double>& delta_internal_vars // (overlapping_node_gids.dims(0), 4 + num_prony_terms)
-    );
-    
-    // debug_cohesive_zone_loads
-    KOKKOS_FUNCTION
-    void debug_cohesive_zone_loads(
-        Mesh_t &mesh,
-        const DCArrayKokkos<double> &pos,
-        const CArrayKokkos<size_t> &overlapping_node_gids,
-        const CArrayKokkos<double> &cohesive_zone_orientation,
-        //const CArrayKokkos<int> &cz_info,
-        //const size_t max_elem_in_cohesive_zone,
-        const ViewCArrayKokkos<double> &internal_vars,
-        const ViewCArrayKokkos<double> &delta_internal_vars,
-        const CArrayKokkos<double> &pair_area,
-        const ViewCArrayKokkos<double> &F_cz
-    );
-    
-    // END OF DEBUG FUNCTION DECLARATIONS
 
     cohesive_zones_t(); 
 
@@ -148,8 +113,7 @@ struct cohesive_zones_t {
     void oriented(
         Mesh_t& mesh,
         DCArrayKokkos<double>& pos,      // reference  coords (num_nodes x 3) 
-        //DCArrayKokkos<double>& pos,    // updated ("t+dt") coords (num_nodes x 3) – can equal pos if not available
-        CArrayKokkos<size_t>& overlapping_node_gids, // (nvcz x 2): A and B node ids per cohesive pair
+        CArrayKokkos<size_t>& overlapping_node_gids, 
         CArrayKokkos<int>& cz_info,      // from build_cohesive_zone_info()
         size_t max_elem_in_cohesive_zone,
         double tol,                 // centroid coincidence tolerance (ABS distance)
@@ -162,16 +126,14 @@ struct cohesive_zones_t {
         const DCArrayKokkos<double>& vel,
         const CArrayKokkos<double>& cohesive_zone_orientation,
         const CArrayKokkos<size_t>& overlapping_node_gids,
-        //const double dt, // timestep driver from sgh_execute.cpp  2/2 comment ouyt
-        const double dt_stage, // 2/2 add
+        const double dt_stage, 
         CArrayKokkos<double>& local_opening    // (overlapping_node_gids.dims(0) x 4): [un_t, utan_t, un_tdt, utan_tdt]
     );
 
     KOKKOS_FUNCTION
     void cohesive_zone_var_update(
         const CArrayKokkos<double>& local_opening,
-        //const double dt, //2/2 comment out
-        const double dt_stage, // 2/2 add
+        const double dt_stage,
         const double time_value, // ADDED IN FOR DEBUGGING
         const CArrayKokkos<size_t>& overlapping_node_gids,
         const RaggedRightArrayKokkos<double>& stress_bc_global_vars,
